f737876b458426eb1d09bd4e770beb3f27b9bab0
SirAntwan <anastaa3@miamioh.edu>
2024-10-16 17:26:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ import streamlit.components.v1 as components
 # Title of the app
 st.title("Drag-and-Drop Survey Builder")
 
-# Survey canvas section
-st.subheader("Survey Canvas")
-
 # Add drag and drop functionality using an iframe with custom HTML
 components.html(f"""
     <div style="display: flex;">
-        <div id="survey-canvas" ondrop="drop(event)" ondragover="allowDrop(event)" style="width: 100%; min-height: 400px; padding: 20px; border: 1px solid black;">
+        <div id="components-list" style="width: 30%; padding: 10px; border-right: 1px solid black;">
+            <h3>Survey Components</h3>
+            <div draggable="true" ondragstart="drag(event)" id="textInput" style="margin: 10px; padding: 10px; background-color: lightgray;">
+                Text Input
+            </div>
+            <div draggable="true" ondragstart="drag(event)" id="checkbox" style="margin: 10px; padding: 10px; background-color: lightgray;">
+                Checkbox
+            </div>
+            <div draggable="true" ondragstart="drag(event)" id="multipleChoice" style="margin: 10px; padding: 10px; background-color: lightgray;">
+                Multiple Choice
+            </div>
+        </div>
+        <div id="survey-canvas" ondrop="drop(event)" ondragover="allowDrop(event)" style="width: 70%; min-height: 400px; padding: 20px; border: 1px solid black;">
             <h3>Survey Canvas</h3>
             <p>Drop components here</p>
         </div>
@@ -33,19 +42,3 @@ components.html(f"""
         }}
     </script>
 """, height=500)
-
-# Add a section to allow users to drag and drop components directly onto the canvas
-st.markdown(
-    """
-    <div draggable="true" ondragstart="drag(event)" id="textInput" style="margin: 10px; padding: 10px; background-color: lightgray;">
-        Text Input
-    </div>
-    <div draggable="true" ondragstart="drag(event)" id="checkbox" style="margin: 10px; padding: 10px; background-color: lightgray;">
-        Checkbox
-    </div>
-    <div draggable="true" ondragstart="drag(event)" id="multipleChoice" style="margin: 10px; padding: 10px; background-color: lightgray;">
-        Multiple Choice
-    </div>
-    """,
-    unsafe_allow_html=True
-)
