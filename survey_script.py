5348cf7e0a8033d6b0db32a3f41997bbd6cc5714
SirAntwan <124811350+SirAntwan@users.noreply.github.com>
2025-02-13 15:13:07 -0500

    Upload survey script

--- survey_script.py
+++ survey_script.py
@@ -6,12 +6,11 @@ import web3
 total_number_pages = 7
 placeholder_buttons = None
 
-Q2_radio_options = ["Yes","No"]
-Q4_radio_options = ["Option 1 multi","Option 2 multi"]
-Q5_radio_options = ["Option 1 Radio","Option 2 Radio"]
-Q6_radio_options = ["Green","Red","Blue","Yellow"]
-Q7_radio_options = ["Yes","No","Ur dumb"]
-Q10_radio_options = ["IDK","Of Course","STFU!!!!!","Inner demons"]
+Q3_radio_options = ["Option 1 multi","Option 2 multi"]
+Q4_radio_options = ["Option 1 Radio","Option 2 Radio"]
+Q5_radio_options = ["Green","Red","Blue","Yellow"]
+Q6_radio_options = ["Yes","No","Ur dumb"]
+Q9_radio_options = ["IDK","Of Course","STFU!!!!!","Inner demons"]
 
 
 # Function that records radio element changes
@@ -48,7 +47,6 @@ if "current_page" not in st.session_state:
     st.session_state["Q7"] = None
     st.session_state["Q8"] = None
     st.session_state["Q9"] = None
-    st.session_state["Q10"] = None
     st.session_state["disabled"] = False
 
 # Page 1; Video
@@ -66,23 +64,11 @@ if st.session_state["current_page"]  == 1:
     st.markdown("""<style> div[class*="stText"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
 
-    st.selectbox(label     = "Selecting Box", 
-             options   = Q2_radio_options, 
-             index     = None if st.session_state["Q2"] == None else st.session_state["Q2"], 
-             key       = 'Q2_radio', 
-             on_change = radio_change, 
-             args      = (Q2_radio_options, "Q2", "Q2_radio",))
-
-    st.markdown("""<style> div[class*="stSelectbox"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
-
-
     placeholder = st.empty()
 
     if st.button('Next', key='next_button_page_1'):
         all_answered = True
         if st.session_state["Q1"] == None:
-            all_answered = False
-        if st.session_state["Q2"] == None:
             all_answered = False
         if all_answered:
             st.session_state["current_page"] += 1
@@ -96,21 +82,20 @@ if st.session_state["current_page"]  == 1:
 
 elif st.session_state["current_page"] == 2:
 
-    st.image("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcTF_OQ101nRiHil4w5295pqLf5M8axplaqmwCthHGgdqlu21bMsP8VmMdl4zkPUnW2pgcNWEWJyl-Y46P1J_d8") 
     st.slider(label="This is a slider question that allows a numeric input between two numbers. This should appear on the second page.",min_value=0,max_value=10, 
-                           value= 5 if st.session_state["Q3"] == None else st.session_state["Q3"], 
-                           key = "Q3_slider",
+                           value= 5 if st.session_state["Q2"] == None else st.session_state["Q2"], 
+                           key = "Q2_slider",
                            on_change = answer_change, 
-                           args = ("Q3", "Q3_slider",))
+                           args = ("Q2", "Q2_slider",))
     st.markdown("""<style> div[class*="stSlider"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
 
     st.multiselect(label     = "This is a multiselect question where you can select multiple answers. This should appear on the second page.", 
-             default   = None if st.session_state["Q4"] == None else st.session_state["Q4"], 
-             options   = Q4_radio_options, 
-             key       = 'Q4_multi', 
+             default   = None if st.session_state["Q3"] == None else st.session_state["Q3"], 
+             options   = Q3_radio_options, 
+             key       = 'Q3_multi', 
              on_change = multi_change, 
-             args      = (Q4_radio_options, "Q4", "Q4_multi",))
+             args      = (Q3_radio_options, "Q3", "Q3_multi",))
 
     st.markdown("""<style> div[class*="stMulti"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
@@ -125,9 +110,9 @@ elif st.session_state["current_page"] == 2:
     with col2:
         if st.button('Next'):
             all_answered = True
-            if st.session_state["Q3"] == None:
+            if st.session_state["Q2"] == None:
                 all_answered = False
-            if st.session_state["Q4"] == None:
+            if st.session_state["Q3"] == None:
                 all_answered = False
             if all_answered:
                 st.session_state["current_page"] += 1
@@ -141,8 +126,17 @@ elif st.session_state["current_page"] == 2:
 
 elif st.session_state["current_page"] == 3:
 
-    st.markdown("""<p style='font-size:18px;'>My Life</p>""", unsafe_allow_html=True)
     st.radio(label     = "This is a multiple choice question where you can choose one of the following. This should appear on the third page.", 
+             options   = Q4_radio_options, 
+             index     = None if st.session_state["Q4"] == None else st.session_state["Q4"], 
+             key       = 'Q4_radio', 
+             on_change = radio_change, 
+             args      = (Q4_radio_options, "Q4", "Q4_radio",))
+
+    st.markdown("""<style> div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
+
+
+    st.radio(label     = "Fav Color?", 
              options   = Q5_radio_options, 
              index     = None if st.session_state["Q5"] == None else st.session_state["Q5"], 
              key       = 'Q5_radio', 
@@ -152,22 +146,12 @@ elif st.session_state["current_page"] == 3:
     st.markdown("""<style> div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
 
-    st.radio(label     = "Fav Color?", 
-             options   = Q6_radio_options, 
-             index     = None if st.session_state["Q6"] == None else st.session_state["Q6"], 
-             key       = 'Q6_radio', 
-             on_change = radio_change, 
-             args      = (Q6_radio_options, "Q6", "Q6_radio",))
-
-    st.markdown("""<style> div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
-
-
     st.multiselect(label     = "Party Time", 
-             default   = None if st.session_state["Q7"] == None else st.session_state["Q7"], 
-             options   = Q7_radio_options, 
-             key       = 'Q7_multi', 
+             default   = None if st.session_state["Q6"] == None else st.session_state["Q6"], 
+             options   = Q6_radio_options, 
+             key       = 'Q6_multi', 
              on_change = multi_change, 
-             args      = (Q7_radio_options, "Q7", "Q7_multi",))
+             args      = (Q6_radio_options, "Q6", "Q6_multi",))
 
     st.markdown("""<style> div[class*="stMulti"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
@@ -182,11 +166,11 @@ elif st.session_state["current_page"] == 3:
     with col2:
         if st.button('Next'):
             all_answered = True
+            if st.session_state["Q4"] == None:
+                all_answered = False
             if st.session_state["Q5"] == None:
                 all_answered = False
             if st.session_state["Q6"] == None:
-                all_answered = False
-            if st.session_state["Q7"] == None:
                 all_answered = False
             if all_answered:
                 st.session_state["current_page"] += 1
@@ -201,18 +185,18 @@ elif st.session_state["current_page"] == 3:
 elif st.session_state["current_page"] == 4:
 
     st.slider(label="Happiness Scale",min_value=0,max_value=100, 
-                           value= 50 if st.session_state["Q8"] == None else st.session_state["Q8"], 
-                           key = "Q8_slider",
+                           value= 50 if st.session_state["Q7"] == None else st.session_state["Q7"], 
+                           key = "Q7_slider",
                            on_change = answer_change, 
-                           args = ("Q8", "Q8_slider",))
+                           args = ("Q7", "Q7_slider",))
     st.markdown("""<style> div[class*="stSlider"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
 
     st.text_area(label     = "Why are you this happy/unhappy?", 
-             value= "" if st.session_state["Q9"] == None else st.session_state["Q9"],
-             key       = 'Q9_text', 
+             value= "" if st.session_state["Q8"] == None else st.session_state["Q8"],
+             key       = 'Q8_text', 
              on_change = answer_change,
-             args      = ( "Q9", "Q9_text",))
+             args      = ( "Q8", "Q8_text",))
 
     st.markdown("""<style> div[class*="stText"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
@@ -227,9 +211,9 @@ elif st.session_state["current_page"] == 4:
     with col2:
         if st.button('Next'):
             all_answered = True
-            if st.session_state["Q8"] == None:
+            if st.session_state["Q7"] == None:
                 all_answered = False
-            if st.session_state["Q9"] == None:
+            if st.session_state["Q8"] == None:
                 all_answered = False
             if all_answered:
                 st.session_state["current_page"] += 1
@@ -266,11 +250,11 @@ elif st.session_state["current_page"] == 5:
 elif st.session_state["current_page"] == 6:
 
     st.radio(label     = "I like cheese?", 
-             options   = Q10_radio_options, 
-             index     = None if st.session_state["Q10"] == None else st.session_state["Q10"], 
-             key       = 'Q10_radio', 
+             options   = Q9_radio_options, 
+             index     = None if st.session_state["Q9"] == None else st.session_state["Q9"], 
+             key       = 'Q9_radio', 
              on_change = radio_change, 
-             args      = (Q10_radio_options, "Q10", "Q10_radio",))
+             args      = (Q9_radio_options, "Q9", "Q9_radio",))
 
     st.markdown("""<style> div[class*="stRadio"] > label > div[data-testid="stMarkdownContainer"] > p {font-size: 18px;}</style> <br><br>""", unsafe_allow_html=True)
 
@@ -285,7 +269,7 @@ elif st.session_state["current_page"] == 6:
     with col2:
         if st.button('Next'):
             all_answered = True
-            if st.session_state["Q10"] == None:
+            if st.session_state["Q9"] == None:
                 all_answered = False
             if all_answered:
                 st.session_state["current_page"] += 1
